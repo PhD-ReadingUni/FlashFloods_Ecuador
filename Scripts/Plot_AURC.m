d3ed5193b532a186417d4b1e3e380561c29ed5ce
FatimaPillosu <fatima.pillosu@ecmwf.int>
2022-01-10 15:08:08 +0000

    New way of computing CTs and their bootstrapped samples.

--- Scripts/Plot_AURC.m
+++ Scripts/Plot_AURC.m
@@ -7,127 +7,203 @@ clear
 clc
 
 % INPUT PARAMETERS
-StepF_S = 18;
-StepF_F = 240;
+StepF_S = 12;
+StepF_F = 12;
 Disc_StepF = 6;
 Acc = 12;
-EFFCI_list = [1,6,10];
-Perc_CDF_RainFF_list = [75,85,90,95,98,99];
+EFFCI_list = [1];
+PercCDF_list = [85];
 SystemFC_list = ["ENS", "ecPoint"];
+NumEM_list = [51,99];
 SystemFCPlot_list = ["r", "b"];
 Region_list = [1,2];
 RegionName_list = ["Costa", "Sierra"];
 RegionPlot_list = ["o-","o--"];
+NumBS = 1000;
 Git_repo = "/vol/ecpoint/mofp/PhD/Papers2Write/FlashFloods_Ecuador";
 DirIN_CT = "Data/Processed/CT_";
 DirOUT_AURC = "Data/Figures/AURC_";
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Plotting the Area Under the Roc Curve (AURC)
+
+% Plotting the Area Under the ROC Curve (AURC)
 disp("Plotting Areas Under the Roc Curve (AURC)")
 
-for indEFFCI = 1 : length(EFFCI_list)
+DirIN = "/vol/ecpoint/mofp/PhD/Papers2Write/FlashFloods_Ecuador/Data/Processed/CT_012/ecPoint/EFFCI01/Perc85/Costa/012";
+FileIN_H = strcat(DirIN, "/CT_BS_H.csv");
+FileIN_FA = strcat(DirIN, "/CT_BS_FA.csv");
+FileIN_M = strcat(DirIN, "/CT_BS_M.csv");
+FileIN_CN = strcat(DirIN, "/CT_BS_CN.csv");
+
+H = import_CT_BS(FileIN_H,"H",1000);
+FA = import_CT_BS(FileIN_FA,"FA",1000);
+M = import_CT_BS(FileIN_M,"M",1000);
+CN = import_CT_BS(FileIN_CN,"CN",1000);
+
+HR = H ./ (H + M);
+FAR = FA ./ (FA + CN);
+
+
+% Computing HR and FAR for ecPoint
+for indBS = 1 : (BS+1)
     
-    % Selecting the EFFCI to consider
-    EFFCI = EFFCI_list(indEFFCI);
-    disp(" ")
-    disp(strcat(" - Plotting AURC for EFFCI>", num2str(EFFCI)))
+    temp_HR = [HR(:,indBS); 1];
+    temp_FAR = [FAR(:,indBS); 1];
     
-    % Defining the output directory
-    DirOUT_temp = strcat(Git_repo, "/", DirOUT_AURC, num2str(Acc), "h/EFFCI", num2str(EFFCI,'%02d'));
-    if ~exist(DirOUT_temp, "dir")
-        mkdir(DirOUT_temp)
-    end
+    figure
+    plot(temp_FAR,temp_HR,"o-")
+    hold on
+    plot([0,1],[0,1],"-k")
     
-    for indPercCDF = 1 : length(Perc_CDF_RainFF_list)
-        
-        % Selecting the percentile that defines the rainfall threshold
-        PercCDF = Perc_CDF_RainFF_list(indPercCDF);
-        disp(strcat("   - PercCDF=", num2str(PercCDF)))
-        
-        % Initiating the figure that will contain the ROC plot and the
-        % variable that will contain the plot legend
-        fig = figure('visible','off');
-        LegendNames = {};
-        
-        for indSystemFC = 1 :length(SystemFC_list)
-            
-            % Selecting the forecasting system to consider
-            SystemFC = SystemFC_list(indSystemFC);
-            SystemFCPlot = SystemFCPlot_list(indSystemFC);
-            
-            for indRegion = 1 : length(Region_list)
-                
-                % Selecting the region to consider
-                Region = Region_list(indRegion);
-                RegionName = RegionName_list(indRegion);
-                RegionPlot = RegionPlot_list(indRegion);
-                
-                % Defining the number of ensemble members (EM) in the
-                % considered forecasting systems
-                if strcmp(SystemFC, "ENS")
-                    NumEM = 51;
-                elseif strcmp(SystemFC, "ecPoint")
-                    NumEM = 99;
-                end
-                
-                % Computing the AURC
-                NumStepF = length(StepF_S:Disc_StepF:StepF_F);
-                AURC = zeros(NumStepF,1);
-                indStepF = 1;
-                for StepF = StepF_S : Disc_StepF : StepF_F
-                    
-                    % Reading the contingency table
-                    File_CT = strcat(Git_repo, "/", DirIN_CT, num2str(Acc), "h/", SystemFC, "/EFFCI", num2str(EFFCI,'%02d'), "/Perc", num2str(PercCDF,'%02d') , "/CT_", RegionName, "_", num2str(StepF,'%03d'), ".csv");
-                    [H,FA,M,CN] = import_CT(File_CT);
-                    
-                    % Computing HR and FAR for ecPoint
-                    HR = [1; (H ./ (H + M))];
-                    FAR  = [1; (FA ./ (FA + CN))];
-                    
-                    % Computing the AURC
-                    AURC_temp = 0;
-                    for i = NumEM : -1 : 2
-                        j = i - 1;
-                        b = HR(j);
-                        B = HR(i);
-                        h = FAR(j)-FAR(i);
-                        AURC_temp = AURC_temp + ((B + b) * h / 2);
-                    end
-                    
-                    AURC(indStepF,1) = AURC_temp;
-                    indStepF = indStepF + 1;
-                    
-                end
-                
-                % Plotting the AURC
-                TypeCurve = strcat(SystemFCPlot,RegionPlot);
-                plot((StepF_S:Disc_StepF:StepF_F)', AURC, TypeCurve, "LineWidth", 1, 'MarkerFaceColor', SystemFCPlot)
-                hold on
-                
-                % Building legend
-                legend_temp = strcat(SystemFC, ", ", RegionName);
-                LegendNames = [LegendNames, {legend_temp}];
-                
-            end
-            
-            % Adding metadata to the plot
-            plot([StepF_S,StepF_F],[0.5,0.5], "k-")
-            xticks((StepF_S:Disc_StepF:StepF_F)')
-            ylim([0.45,1])
-            xlim([StepF_S StepF_F])
-            title([strcat("AURC (PercCDF = ", num2str(PercCDF,'%02d'), "th percentile)"), strcat("EFFCI >= ",num2str(EFFCI,'%02d'))],'FontSize',16)
-            xlabel("End 12-h accumulation period (hours)",'FontSize',14)
-            ylabel("AURC",'FontSize',14)
-            %legend(LegendNames, 'Location','southeast','FontSize',12)
-            grid on
-            
-            % Saving the figures as .eps
-            FileOUT = strcat(DirOUT_temp, "/AURC_PercCDF_", num2str(PercCDF,'%02d'), ".eps");
-            saveas(fig, FileOUT, "epsc")
-            
-        end
-        
+    % Computing the AURC
+    AURC_temp = 0;
+    NumPoints = length(temp_HR);
+    for i = 1 : (NumPoints-1)
+        j = i + 1;
+        b = temp_HR(i);
+        B = temp_HR(j);
+        h = temp_FAR(j)-temp_FAR(i);
+        AURC_temp = AURC_temp + (((B + b) * h) / 2);
     end
     
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% 
+% for indEFFCI = 1 : length(EFFCI_list)
+%     
+%     % Selecting the EFFCI to consider
+%     EFFCI = EFFCI_list(indEFFCI);
+%     disp(" ")
+%     disp(strcat(" - Plotting AURC for EFFCI>", num2str(EFFCI)))
+%     
+%     % Defining the output directory
+%     DirOUT_temp = strcat(Git_repo, "/", DirOUT_AURC, num2str(Acc), "h/EFFCI", num2str(EFFCI,'%02d'));
+%     if ~exist(DirOUT_temp, "dir")
+%         mkdir(DirOUT_temp)
+%     end
+%     
+%     for indPercCDF = 1 : length(Perc_CDF_RainFF_list)
+%         
+%         % Selecting the percentile that defines the rainfall threshold
+%         PercCDF = Perc_CDF_RainFF_list(indPercCDF);
+%         disp(strcat("   - PercCDF=", num2str(PercCDF)))
+%         
+%         % Initiating the figure that will contain the ROC plot and the
+%         % variable that will contain the plot legend
+%         %fig = figure('visible','off');
+%         figure
+%         LegendNames = {};
+%         
+%         for indSystemFC = 1 :length(SystemFC_list)
+%             
+%             % Selecting the forecasting system to consider
+%             SystemFC = SystemFC_list(indSystemFC);
+%             SystemFCPlot = SystemFCPlot_list(indSystemFC);
+%             disp(SystemFC)
+%             
+%             
+%             for indRegion = 1 : length(Region_list)
+%                 
+%                 % Selecting the region to consider
+%                 Region = Region_list(indRegion);
+%                 RegionName = RegionName_list(indRegion);
+%                 RegionPlot = RegionPlot_list(indRegion);
+%                 disp(Region)
+%                 
+%                 
+%                 % Defining the number of ensemble members (EM) in the
+%                 % considered forecasting systems
+%                 if strcmp(SystemFC, "ENS")
+%                     NumEM = 51;
+%                 elseif strcmp(SystemFC, "ecPoint")
+%                     NumEM = 99;
+%                 end
+%                 
+%                 % Computing the AURC
+%                 NumStepF = length(StepF_S:Disc_StepF:StepF_F);
+%                 AURC = zeros(NumStepF,1);
+%                 indStepF = 1;
+%                 
+%                 for StepF = StepF_S : Disc_StepF : StepF_F
+%                     disp(StepF)
+%                     
+%                     % Average plot
+%                     NumDays_list = (1:NumDays);
+%                     H = zeros(NumEM,1);
+%                     FA = zeros(NumEM,1);
+%                     M = zeros(NumEM,1);
+%                     CN = zeros(NumEM,1);
+%                     
+%                     for indDays = 1 : NumDays
+%                         
+%                         Day = NumDays_list(indDays);
+%                         
+%                         % Reading the contingency table
+%                         File_CT = strcat(Git_repo, "/", DirIN_CT, num2str(Acc, "%03.f"), "/", SystemFC, "/EFFCI", num2str(EFFCI,'%02d'), "/Perc", num2str(PercCDF,'%02d') , "/", RegionName, "/", num2str(StepF,'%03d'), "/CT_Day", num2str(Day, "%03.f"), ".csv");
+%                         [H_temp,FA_temp,M_temp,CN_temp] = import_CT(File_CT);
+%                         H = H + H_temp;
+%                         FA = FA + FA_temp;
+%                         M = M + M_temp;
+%                         CN = CN + CN_temp;
+%                         
+%                     end
+%                         
+%                     
+%                 
+%                 % Plotting the AURC
+%                 TypeCurve = strcat(SystemFCPlot,RegionPlot);
+%                 plot((StepF_S:Disc_StepF:StepF_F)', AURC, TypeCurve, "LineWidth", 1, 'MarkerFaceColor', SystemFCPlot)
+%                 hold on
+%                 
+%                 % Building legend
+%                 legend_temp = strcat(SystemFC, ", ", RegionName);
+%                 LegendNames = [LegendNames, {legend_temp}];
+%                 
+%             end
+%             
+%             % Adding metadata to the plot
+%             plot([StepF_S,StepF_F],[0.5,0.5], "k-")
+%             xticks((StepF_S:Disc_StepF:StepF_F)')
+%             ylim([0,1])
+%             xlim([StepF_S StepF_F])
+%             title([strcat("AURC (PercCDF = ", num2str(PercCDF,'%02d'), "th percentile)"), strcat("EFFCI >= ",num2str(EFFCI,'%02d'))],'FontSize',16)
+%             xlabel("End 12-h accumulation period (hours)",'FontSize',14)
+%             ylabel("AURC",'FontSize',14)
+%             %legend(LegendNames, 'Location','southeast','FontSize',12)
+%             grid on
+%             
+% %             % Saving the figures as .eps
+% %             FileOUT = strcat(DirOUT_temp, "/AURC_PercCDF_", num2str(PercCDF,'%02d'), ".eps");
+% %             saveas(fig, FileOUT, "epsc")
+% %             
+%         end
+%         
+%     end
+%     
+% end
--- Scripts/Test1.m
+++ Scripts/Test1.m
@@ -1,6 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Compute_CT.m computes the contingency tables for flood verification. 
-
+% Compute_CT.m computes the contingency tables for flood verification using 
+% forecast runs from 00 and 12 UTC. 
+%
 % NOTE: This script is hard-coded for forecasts with runs at 00 UTC and 12 
 % UTC. If someone wants to use it with other run times, the for loop at
 % line 103 should be modified accordingly.
@@ -12,23 +13,27 @@ clc
 % INPUT PARAMETERS
 Year = 2020;
 StepF_S = 12;
-StepF_F = 24;
+StepF_F = 12;
 DiscStep = 6;
 Acc = 12;
+
+EFFCI_list = [1];
+
+PercCDF_list = [85];
+PercRetentionFR = 25;
+
 SystemFC_list = ["ENS","ecPoint"];
 NumEM_list = [51,99];
+
 Region_list = [1,2];
 RegionName_list = ["Costa","Sierra"];
-EFFCI_list = [1,6,10];
-PercCDF_list = [85,90,95,98,99];
-PercRetentionFR = 25;
+
 Git_repo = "/vol/ecpoint/mofp/PhD/Papers2Write/FlashFloods_Ecuador";
 FileIN_Emask = "Data/Raw/EcuadorMasks/Emask_ENS.csv";
 DirIN_FC = "Data/Raw/FC_Emask_Rainfall_";
 DirIN_FR = "Data/Raw/GridAccFR_";
 DirIN_CDF = "Data/Processed/RainCDF_";
 DirOUT = "Data/Processed/CT_";
-LZ_Acc = 3;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -48,42 +53,43 @@ dS=datenum(BaseDateS,'yyyy-mm-dd');
 dF=datenum(BaseDateF,'yyyy-mm-dd');
 
 % Creating strings from numbers
-AccSTR = num2str(Acc, strcat('%0',num2str(LZ_Acc),'.f'));
+AccSTR = num2str(Acc, strcat('%03.f'));
 
 
 % Computing the contingency tables 
-for indSystemFC = 1: length(SystemFC_list)
+for indEFFCI = 1 : length(EFFCI_list)
     
-    % Selecting the forecasting system to consider
-    SystemFC = SystemFC_list(indSystemFC);
-    NumEM = NumEM_list(indSystemFC);
-    disp(strcat(" - Considering the ", SystemFC, " forecasting system"))
+    % Selecting the EFFCI threshold to consider
+    EFFCI = EFFCI_list(indEFFCI);
+    disp(" ")
+    disp(strcat("   - Considering the flood reports with EFFCI>=",num2str(EFFCI)))
     
-    for indRegion = 1 : length(Region_list)
+    for indPercCDF = 1 : length(PercCDF_list)
         
-        % Selecting the region to consider
-        Region = Region_list(indRegion);
-        RegionName = RegionName_list(indRegion);
-        disp(strcat("  - Considering the '", RegionName, "' region"))
-        
-        % Selecting the points that belong to the considered region in the
-        % Ecuador's domain
-        pointer_region = find(Emask(:,3)==Region);
-        
-        for indEFFCI = 1 : length(EFFCI_list)
-            
-            % Selecting the EFFCI threshold to consider
-            EFFCI = EFFCI_list(indEFFCI);
+        % Selecting the rainfall event to verify
+        PercCDF = PercCDF_list(indPercCDF);
+        disp(" ")
+        disp(strcat("    - Considering rainfall events >= (PercCDF=", num2str(PercCDF), "th percentile)"))
+
+        for indSystemFC = 1: length(SystemFC_list)
+    
+            % Selecting the forecasting system to consider
+            SystemFC = SystemFC_list(indSystemFC);
+            NumEM = NumEM_list(indSystemFC);
             disp(" ")
-            disp(strcat("   - Considering the flood reports with EFFCI>=",num2str(EFFCI)))
+            disp(strcat(" - Considering the ", SystemFC, " forecasting system"))
             
-            for indPercCDF = 1 : length(PercCDF_list)
-                
-                % Selecting the percentile that defines how extreme is the
-                % rainfall event to verify 
-                PercCDF = PercCDF_list(indPercCDF);
+            for indRegion = 1 : length(Region_list)
+        
+                % Selecting the region to consider
+                Region = Region_list(indRegion);
+                RegionName = RegionName_list(indRegion);
                 disp(" ")
-                disp(strcat(" - Considering rainfall events >= (PercCDF=", num2str(PercCDF), "th percentile)"))
+                disp(strcat("  - Considering the '", RegionName, "' region"))
+                
+                % Selecting the points that belong to the considered region 
+                % in the Ecuador's domain
+                pointer_region = find(Emask(:,3)==Region);
                 
                 % Reading the rainfall values that define the verification
                 % events
@@ -96,38 +102,36 @@ for indSystemFC = 1: length(SystemFC_list)
                 
                 % Selecting the step of end accumulation period to consider
                 % for forecasts from the 00 UTC run
-                DiscRunTime = 12;
                 for StepF00 = StepF_S : DiscStep : StepF_F
                     
-                    disp(strcat("   - StepF=", num2str(StepF00)))
+                    disp(strcat("     - StepF=", num2str(StepF00)))
+                    
+                    % Creating output directory
+                    DirOUT_temp = strcat(Git_repo, "/", DirOUT, AccSTR, "/", SystemFC, "/EFFCI", num2str(EFFCI,'%02.f'), "/Perc", num2str(PercCDF,'%02.f'), "/", RegionName, "/", num2str(StepF00,'%03.f'));
+                    if ~exist(DirOUT_temp, "dir")
+                        mkdir(DirOUT_temp)
+                    end
+                    
                     StepS00 = StepF00 - Acc;
-                    NumDay = 1;
+                    
+                    H = zeros(NumEM,366);
+                    FA = zeros(NumEM,366);
+                    M = zeros(NumEM,366);
+                    CN = zeros(NumEM,366);
+                    
+                    CountDay = 1;
                     
                     for d00 = dS : dF
                         
-                        % Creating template for the contingency tables
-                        CT = zeros(NumEM,4);
-                    
-                        % Selecting the step at the begining of the 
-                        % accumulation period to consider for 12 UTC runs 
-                        if StepS00 < DiscRunTime
+                        disp(datestr(d00))
+                        if StepS00 < 12
                             d12 = d00 - 1;
-                            StepS12 = StepS00 + DiscRunTime;
+                            StepS12 = StepS00 + 12;
                         else
                             d12 = d00;
-                            StepS12 = StepS00 - DiscRunTime;
+                            StepS12 = StepS00 - 12;
                         end
-                        
-                        % Determining the average step of end of the 
-                        % accumulation period between the 00 and 12 UTC run
                         StepF12 = StepS12 + Acc;
-                        AvStepF = (StepF00 + StepF12) / 2;
-                        
-                        % Creating output directory
-                        DirOUT_temp = strcat(Git_repo, "/", DirOUT, AccSTR, "/", SystemFC, "/EFFCI", num2str(EFFCI,'%02.f'), "/Perc", num2str(PercCDF,'%02.f'), "/", RegionName, "/", num2str(AvStepF,'%03.f'));
-                        if ~exist(DirOUT_temp, "dir")
-                            mkdir(DirOUT_temp)
-                        end
                         
                         % Select the forecasts to consider
                         FileIN_FC00 = strcat(Git_repo, "/", DirIN_FC, AccSTR, "/", SystemFC, "/", datestr(d00, "yyyymmdd"), "00/tp_", num2str(Acc,'%03d'), "_", datestr(d00, "yyyymmdd"), "_00_", num2str(StepF00,'%03.f'), ".csv");
@@ -137,8 +141,9 @@ for indSystemFC = 1: length(SystemFC_list)
                         ValidTime = d00 + (0/24) + (StepF00/24);
                         FileIN_FR = strcat(Git_repo, "/", DirIN_FR, AccSTR, "/EFFCI", num2str(EFFCI,'%02.f'), "/GridAccFR_", datestr(ValidTime, 'yyyymmdd'), "_", datestr(ValidTime, 'HH'), ".csv");
                         
-                        % Reading the forecasts and the verifying observations
-                        if isfile(FileIN_FC00) && isfile(FileIN_FC12)
+                        % Reading the forecasts and the verifying 
+                        % observations if they exist 
+                        if isfile(FileIN_FC00) && isfile(FileIN_FC12) && isfile(FileIN_FR)
                             
                             FC00 = import_FC(FileIN_FC00,SystemFC);
                             FC12 = import_FC(FileIN_FC12,SystemFC);
@@ -146,39 +151,42 @@ for indSystemFC = 1: length(SystemFC_list)
                             
                             FR_temp = import_GridAccFR(FileIN_FR);
                             FR = [FR_temp(pointer_region,3); FR_temp(pointer_region,3)];
-                            
-                            % Defining how many events exceed the rainfall threshold
-                            FC_exceed = (FC >= rev);
-                            FC_exceed_total = sum(FC_exceed,2);
-                            
+                                
+                            % Defining how many events exceed the rainfall 
+                            % threshold
+                            EMs_exceedFC = (FC >= rev);
+                            NumEMs_exceedFC = sum(EMs_exceedFC,2);
+                                
                             % Defining the contingency table
-                            for indEM = 1 : NumEM
+                            for ind_NumEM = 1 : NumEM
                                 
-                                indCT = NumEM - indEM + 1;
+                                indCT = NumEM - ind_NumEM + 1;
                                 
-                                pointer_FC_YES = find(FC_exceed_total>=indEM);
-                                pointer_FC_NO = find(FC_exceed_total<indEM);
+                                pointer_FC_YES = find(NumEMs_exceedFC>=ind_NumEM);
+                                pointer_FC_NO = find(NumEMs_exceedFC<ind_NumEM);
                                 
                                 FR_FC_YES = FR(pointer_FC_YES);
                                 FR_FC_NO = FR(pointer_FC_NO);
                                 
-                                CT(indCT,1) = sum(FR_FC_YES==1); % hits (H)
-                                CT(indCT,2) = sum(FR_FC_YES==0); % false alarms (FA)
-                                CT(indCT,3) = sum(FR_FC_NO==1); % misses (M)
-                                CT(indCT,4) = sum(FR_FC_NO==0); % correct negatives (CN)
+                                H(indCT,CountDay) = sum(FR_FC_YES==1);
+                                FA(indCT,CountDay) = sum(FR_FC_YES==0);
+                                M(indCT,CountDay) = sum(FR_FC_NO==1);
+                                CN(indCT,CountDay) = sum(FR_FC_NO==0);
                                 
                             end
                             
-                            % Saving contingecy table
-                            CT = array2table(CT,'VariableNames', {'H','FA','M','CN'});
-                            FileOUT = strcat(DirOUT_temp,"/CT_Day", num2str(NumDay,'%03.f'), ".csv");
-                            writetable(CT,FileOUT,'Delimiter',',')
+                            CountDay = CountDay + 1;
                             
-                            NumDay = NumDay + 1;
-                        
                         end
                         
                     end
+                    
+                    % Removing days for which forecasts and/or verifying
+                    % observations were not available
+                    H(:,all(~(H),1)) = [];
+                    FA(:,all(~(FA),1)) = [];
+                    M(:,all(~(M),1)) = [];
+                    CN(:,all(~(CN),1)) = [];
                     
                 end
                 
